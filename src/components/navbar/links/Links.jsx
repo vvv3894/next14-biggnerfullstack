cf78b0abdebce0f85547736c9237ea4039bc0a24
vvv3894 <gkgk3894@gmail.com>
2024-10-12 15:58:20 +0900

    47:49

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,37 @@
+import styles from "./links.module.css";
+import NavLink from "./navLink/navLink";
+
+const { default: Link } = require("next/link");
+
+const Links = () => {
+
+    const links =[
+        {
+            title: "Homepage",
+            path:"/",
+        },
+        {
+            title: "About",
+            path:"/about",
+        },
+        {
+            title: "Contact",
+            path:"/contact",
+        },
+        {
+            title: "Blog",
+            path:"/blog",
+        }
+
+    ];
+
+  return (
+    <div className={styles.links}>
+        {links.map((link=>(
+            <NavLink item={link} key={link.title}/>
+        )))}
+       </div>
+       );
+};
+
+export default Links;
