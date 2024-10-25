8462645974a10d6b1839abdc0ded23daffd8abc0
vvv3894 <gkgk3894@gmail.com>
2024-10-26 00:04:05 +0900

    1:26:11 blog page ,mogodb mogoose 전까지

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,38 @@ import styles from "./about.module.css";
 
 const AboutPage = () => {
     return (
-        <div>
+        <div className={styles.container}>
+            <div className={styles.textContainer}>
+                <h2 className={styles.subtitle}>About Agency</h2>
+                <h1 className={styles.title}>콘텐츠의 바다속에서 덕후들을 위한 도서관</h1>
+                <p className={styles.desc}>콘텐츠의 바다속에서 덕후들을 위한 도서관이랄까 뭐랄까
+                    콘텐츠의 바다속에서 덕후들을 위한 도서관이랄까 뭐랄까
+                    콘텐츠의 바다속에서 덕후들을 위한 도서관이랄까 뭐랄까
+                    콘텐츠의 바다속에서 덕후들을 위한 도서관이랄까 뭐랄까
+                </p>
+                <div className={styles.boxes}>
+                    <div className={styles.box}>
+                        <h1>100 K</h1>
+                        <p>Year of experince</p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>100 K</h1>
+                        <p>Year of experince</p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>100 K</h1>
+                        <p>Year of experince</p>
+                    </div>
+                </div>
+
+            </div>
             <div className={styles.imgContainer}>
-            <Image src="/about.png" alt="fill" width={500} height={500}/>
+                <Image 
+                  src="/about.png"
+                  alt="About Image"
+                  fill
+                  className={styles.img}
+                />
             </div>
         </div>
     //   넥스트js는 html처럼 주소로 이미지 태그에서 바로 불러오기 불가능 콘피그에서..
